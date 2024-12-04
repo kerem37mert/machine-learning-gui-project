a1f28aa28509f6e3410f2a4387a14fd2790a83e7
kerem37mert <kerem37mert@hotmail.com>
2024-12-04 12:53:51 +0300

    balanced dataset added

---
+++ ML/balanced_dataset.py
@@ -0,0 +1,26 @@
+from imblearn.over_sampling import SMOTE
+import pandas as pd
+
+# dengesiz veri setini parametre olarak alır.
+# veri setindeki sayısı az olan sınıfa ait örnekler çoğaltılrı.
+# veri seti dengele hale getirilir ve geriye döndürülür.
+
+def balanced_dataset(dataset):
+    X = dataset.drop('target', axis=1)  # 'class' hedef değişkeni olduğu varsayılıyor
+    y = dataset['target']
+    # print(X)
+    # print(y)
+
+    # smote işlemi uygula ve veri setini dengeli hale getir
+    smote = SMOTE(sampling_strategy='auto')
+    X_res, y_res = smote.fit_resample(X, y)
+
+    # X_res'i  Pandas DataFrame'e dönüştürme
+    X_res_df = pd.DataFrame(X_res, columns=X.columns)
+    # y_res'i Pandas DataFrame'e dönüştürme
+    y_res_df = pd.DataFrame(y_res, columns=['target'])
+
+    # Yeni dengelemiş veri setini birleştirme
+    df_resampled = pd.concat([X_res_df, y_res_df], axis=1)
+
+    return df_resampled
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PyQt5 import QtWidgets, uic
 import modelMenu
 import pandas as pd
 
+from ML.balanced_dataset import balanced_dataset
 from ML.unbalanced_dataset import unbalanced_dataset
 
 
@@ -14,9 +15,11 @@ class Ui_Main(QtWidgets.QDialog):
         # veri setlerini oku
         self.dataset1 = pd.read_csv("heart.csv") # orijinal veri seti
         self.dataset2 = unbalanced_dataset(self.dataset1) # dengesiz veri seti
+        self.dataset3 = balanced_dataset(self.dataset2) # dengeli veri seti (Smote uygulandı)
 
         self.button1.clicked.connect(lambda: self.go_to_model_menu(self.dataset1, "Orijinal Veriler"))
         self.button2.clicked.connect(lambda: self.go_to_model_menu(self.dataset2, "Dengesiz Veriler"))
+        self.button3.clicked.connect(lambda: self.go_to_model_menu(self.dataset3, "Dengeli Veriler"))
 
     def go_to_model_menu(self, dataset, title):
         # Menu penceresine geçiş yap
