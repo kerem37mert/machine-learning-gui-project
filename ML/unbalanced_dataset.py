80fcaabf548a4a8d790fb2fa281d140fb32bfb31
kerem37mert <kerem37mert@hotmail.com>
2025-01-02 11:11:10 +0300

    bug fixed

--- ML/unbalanced_dataset.py
+++ ML/unbalanced_dataset.py
@@ -1,7 +1,7 @@
 def unbalanced_dataset(dataset):
 
     target_0 = dataset[dataset['target'] == 0] # veri setindeki target değeri 0 olanları getir
-    target_0_removed = target_0.sample(n=400)  # sınıfı 0 olanlardan rastgele 200 tane seç
+    target_0_removed = target_0.sample(n=400)  # sınıfı 0 olanlardan rastgele 400 tane seç
 
     new_dataset = dataset.drop(target_0_removed.index) # yeni veri seti
 
